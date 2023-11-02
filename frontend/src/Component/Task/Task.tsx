a6fa54bc0cb8f689ed5febaa0e3ecb66baddf78c
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-11-03 02:19:59 +0600

    todo, in-progress, done div create

--- frontend/src/Component/Task/Task.tsx
+++ frontend/src/Component/Task/Task.tsx
@@ -1,13 +1,31 @@
 import { FC } from 'react';
+import style from "./Task.module.css"
 
 interface TaskProps {
 
 };
 
 const Task: FC<TaskProps> = ({ }) => {
+
+
+
     return (
         <div>
-            Task
+            <div className={style.task_h1_tag}>
+                <h1>Task</h1>
+            </div>
+            <div className={style.task_all_tasks}>
+                <div id='todo'>
+                    todo
+                </div>
+
+                <div id='in-progress'>
+                    doing
+                </div>
+                <div id='done'>
+                    done
+                </div>
+            </div>
         </div>
     );
 };
