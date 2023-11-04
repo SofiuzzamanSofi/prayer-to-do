9647049a6e1403d3f3447e3215c6a14ea63ba1e0
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-11-04 08:15:23 +0600

    animatin added gif file

--- frontend/src/Component/LoadingPage/LoadingPage.tsx
+++ frontend/src/Component/LoadingPage/LoadingPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import loadingImage from "../../assets/loadingBlackBg.gif"
+import loadingImage from "../../assets/Animation .gif"
 import style from "./LoadingPage.module.css"
 
 interface LoadingPageProps {
@@ -8,7 +8,7 @@ interface LoadingPageProps {
 
 const LoadingPage: FC<LoadingPageProps> = ({ }) => {
     return (
-        <div>
+        <div className={style.loading_image_div}>
             <img className={style.loading_image} src={loadingImage} alt="loading-image" />
         </div>
     );
